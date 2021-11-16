205304e320aa427ec2294d157bdb417d2a1bae8e
lingram96 <lewisingram.dev@outlook.com>
2021-11-16 19:29:40 +0000

    first commit

--- src/01-count-major-versions-above-10/index.js
+++ src/01-count-major-versions-above-10/index.js
@@ -28,9 +28,23 @@ The results should have this structure:
  *  the number of packages that have a MAJOR semver version 
  *  greater than 10.x.x
  */
+const axios = require('axios')
 
 module.exports = async function countMajorVersionsAbove10() {
   // TODO
+let count = 0
 
+const response = await axios.post("http://ambush-api.inyourarea.co.uk/ambush/intercept", {
+     "url": "https://api.npms.io/v2/search/suggestions?q=react",
+     "method": "GET",
+     "return_payload": true
+ }).then(data => data.data.content)
+ .catch(error => console.log(error))
+
+ for(let item of response){
+     if(item.package.version.split('.')[0] > 10)
+     count += 1
+ }
+ 
   return count
 };
--- src/02-oldest-package-name/index.js
+++ src/02-oldest-package-name/index.js
@@ -27,9 +27,26 @@ The results should have this structure:
  *  With the results from this request, inside "content", return
  *  the "name" of the package that has the oldest "date" value
  */
+const axios = require('axios')
 
 module.exports = async function oldestPackageName() {
   // TODO
 
-  return name
+const allPackages = await axios.post("http://ambush-api.inyourarea.co.uk/ambush/intercept", {
+    "url": "https://api.npms.io/v2/search/suggestions?q=react",
+    "method": "GET",
+    "return_payload": true
+}).then(data => data.data.content)
+.catch(error => console.log(error))
+
+let oldest = allPackages[0]
+
+for(const item of allPackages){
+  if(item.package.date < oldest.package.date){
+    oldest = item
+  }
+}
+const name = oldest.package.name
+
+return name
 };
